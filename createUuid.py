769367228e29836d3cddfc9261840d765b1a5406
Daniel Furman <djfurman@gmail.com>
2017-10-01 13:50:09 -0400

    Refactor to gateway response method.

--- createUuid.py
+++ createUuid.py
@@ -1,10 +1,16 @@
 from uuid import uuid4
+from json import dumps
 
 
 def lambda_handler(event, context):
+    body = {'uuid': str(uuid4())}
+    return returnGatewayResponse(body)
+
+
+def returnGatewayResponse(body):
     return {
         'isBase64Encoded': False,
         'statusCode': 200,
         'headers': {'content-type': 'application/json'},
-        'body': str({'uuid': str(uuid4())})
+        'body': str(dumps(body))
     }
